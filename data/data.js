be12dfca64b2ed28f1551f88ca5d5deed95b02ee
kekc <kekcsik009@gmail.com>
2025-06-11 04:17:55 +0700

    Hw1

---
+++ data/data.js
@@ -0,0 +1,17 @@
+﻿export const cardList = [
+  {
+    id: "1",
+    title: "Пример задачи",
+    theme: "web",
+    date: "2024-01-01",
+    status: "Без статуса",
+    text: "Описание"
+  }
+  // ... другие карточки
+];
+
+export const statusList = [
+  { id: "1", status: "Без статуса" },
+  { id: "2", status: "В работе" },
+  { id: "3", status: "Готово" }
+];
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import ModalWin from "../Modalka/Modalka";
+import PopNewCard from "../PopNewCard/PopNewCard";
+import {
+  HeaderBlock,
+  HeaderBtnMainNew,
+  HeaderContainer,
+  HeaderLogo,
+  HeaderNav,
+  HeaderUser,
+  SHeader,
+} from "./SHeader";
+
+const Header = ({ addNewCard, cards }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isPopNewCardOpen, setIsPopNewCardOpen] = useState(false);
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+  const openPopNewCard = () => {
+    setIsPopNewCardOpen(true);
+  };
+
+  return (
+    <SHeader>
+      <HeaderContainer>
+        <HeaderBlock>
+          <HeaderLogo>
+            <a href="" target="_self">
+              <img src="../assets/logo.png" alt="logo" />
+            </a>
+          </HeaderLogo>
+          <HeaderLogo>
+            <a href="" target="_self">
+              <img src="../assets/logo_dark.png" alt="logo" />
+            </a>
+          </HeaderLogo>
+          <HeaderNav>
+            <HeaderBtnMainNew onClick={openPopNewCard} id="btnMainNew">
+              <a href="#popNewCard">Создать новую задачу</a>
+            </HeaderBtnMainNew>
+            <HeaderUser onClick={toggleModal}>
+              Ivan Ivanov
+            </HeaderUser>
+            {isModalOpen && <ModalWin />}
+            {isPopNewCardOpen && (
+              <PopNewCard
+                addNewCard={addNewCard}
+                cards={cards}
+                isPopNewCardOpen={isPopNewCardOpen}
+                setIsPopNewCardOpen={setIsPopNewCardOpen}
+              />
+            )}
+          </HeaderNav>
+        </HeaderBlock>
+      </HeaderContainer>
+    </SHeader>
+  );
+};
+
+export default Header;
