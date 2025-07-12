badbc6104926301b6eb86ffbc2594192695afc55
kekc <kekcsik009@gmail.com>
2025-07-12 21:29:26 +0700

    start dz3

--- data/data.js
+++ data/data.js
@@ -6,13 +6,47 @@
     date: "2024-01-01",
     status: "Без статуса",
     text: "Описание"
+  },
+  {
+    id: "2",
+    title: "Toxic",
+    theme: "web",
+    date: "2024-01-01",
+    status: "Нужно сделать",
+    text: "Описаниеs"
+  },
+  {
+    id: "3",
+    title: "Toxic",
+    theme: "web",
+    date: "2024-01-01",
+    status: "В работе",
+    text: "Описаниеs"
+  },
+  {
+    id: "4",
+    title: "Toxic",
+    theme: "web",
+    date: "2024-01-01",
+    status: "Тестирование",
+    text: "Описаниеs"
+  },
+  {
+    id: "5",
+    title: "Toxic",
+    theme: "web",
+    date: "2024-01-01",
+    status: "Готово",
+    text: "Описаниеs"
   }
 ];
 
 export const statusList = [
   { id: "1", status: "Без статуса" },
-  { id: "2", status: "В работе" },
-  { id: "3", status: "Готово" }
+  { id: "2", status: "Нужно сделать" },
+  { id: "3", status: "В работе" },
+  { id: "4", status: "Тестирование" },
+  { id: "5", status: "Готово" }
 ];
 
 // dz 2
